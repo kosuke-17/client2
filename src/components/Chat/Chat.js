c81fc4c20036413e0be477236560a0b51e8cffc2
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 01:16:57 +0900

    メッセージ機能の追加

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+
 let socket;
+
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -17,12 +26,44 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room});
+    socket.emit('join', { name, room}, () => {
+
+    });
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    }
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages)
+
+  // function for sending messages
+
   return (
-    <h1>Chat</h1>
-  )
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room}/>
+
+        <Input message={message} setMessage={setMessage} send={sendMessage} />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
