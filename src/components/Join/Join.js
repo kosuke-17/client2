8638192b7b3cd7a0aa55de4fcd029df5e88d84d8
kosuke-17 <tamutamu17@icloud.com>
2021-05-15 16:29:15 +0900

    Socketの利用

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -9,14 +9,18 @@ const Join = () => {
 
   return (
     <div className="joinOuterContainer">
-    <div className="joinInnerContainer">
-      <h1 className="heading">Join</h1>
-      <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
-      <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
-      <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-        <button className="button mt-20" type="submit">ルームを作る</button>
-      </Link>
-    </div>
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div>
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+        </div>
+        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className="button mt-20" type="submit">ルームを作る</button>
+        </Link>
+      </div>
     </div>
   );
 }
